4b30840962e78262f1bb9670e2cd66ddf3ed88be
Auke van Scheltinga <aukevs@gmail.com>
2012-06-04 15:21:08 +0200

    i18n lesson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+  
+  def set_locale
+    I18n.locale = get_locale_from_browser_setting 
+  end
+  
+  def get_locale_from_browser_setting
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+  
 end
