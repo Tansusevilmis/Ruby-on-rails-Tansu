716fb3102cbd0599bda59cd2414e107964098bc1
Auke van Scheltinga <aukevs@gmail.com>
2012-06-11 12:59:43 +0200

    twitter api intregration

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'devise'
 
 gem 'json'
 
+gem 'twitter'
+
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,11 @@ class WelcomeController < ApplicationController
   before_filter :authenticate_user!
   
   def show
-
+    @tweets = Twitter.user_timeline("vanscheltinga")
   end
   
   def destroy
     
   end
-  
+   
 end
